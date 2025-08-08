e9f3e11aa7c194d1894b963f58b80ad3f17e51fa
Betty <bettypuramana@gmail.com>
2025-08-08 11:49:59 +0530

    tables

---
+++ database/migrations/2025_08_08_061316_create_enquiries_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enquiries', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->integer('passengers')->default(1);
+            $table->date('travel_date')->nullable();
+            $table->string('name');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enquiries');
+    }
+};
